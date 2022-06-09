54a494a1d34502b35ff7b478fcccfedfa7d3ed3c
Kristine Kochie <kristinekochie@gmail.com>
2022-06-09 08:37:35 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const cookbook = {
+    title: "The Book on Pie",
+    author: "Erin Jeanne McDowell",
+    subject: "Baking",
+}
+
+const nonFiction = {
+    title: "Crying in H-Mart",
+    author: "Michelle Zauner",
+    genre: "Memoir",
+}
+
+const fiction = {
+    title: "Cloud Cuckoo Land",
+    author: "Anthony Doerr",
+    genre: "Historical Fiction",
+}
