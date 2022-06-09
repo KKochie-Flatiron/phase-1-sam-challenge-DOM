acc7359c2546bef7632195fdb472d575af51ceb3
Kristine Kochie <kristinekochie@gmail.com>
2022-06-09 09:09:14 -0400

    Add JS to create book list

--- script.js
+++ script.js
@@ -15,3 +15,39 @@ const fiction = {
     author: "Anthony Doerr",
     genre: "Historical Fiction",
 }
+
+function createBook(book) {
+    const li = document.createElement('li');
+    const h3 = document.createElement('h3');
+    const h4 = document.createElement('h4');
+    const p = document.createElement('p');
+
+    h3.innerText = book.title;
+    h4.innerText = book.author;
+    p.innerText = book.genre;
+
+    li.append(h3, h4, p);
+    document.querySelector('ul').append(li);
+}
+
+function createCookbook(book) {
+    const li = document.createElement('li');
+    const h3 = document.createElement('h3');
+    const h4 = document.createElement('h4');
+    const p = document.createElement('p');
+
+    h3.innerText = book.title;
+    h4.innerText = book.author;
+    p.innerText = book.subject;
+
+    li.className = 'cooking';
+
+    li.append(h3, h4, p);
+    document.getElementById('recent-books').append(li);
+}
+
+
+createBook(fiction);
+createBook(nonFiction);
+
+
