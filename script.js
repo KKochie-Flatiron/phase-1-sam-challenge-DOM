cc95a126b6d1436f2ec32f24ea86f0b2406176d2
Kristine Kochie <kristinekochie@gmail.com>
2022-06-12 23:14:29 -0400

    Add db.json and modified js file

--- script.js
+++ script.js
@@ -1,79 +1,45 @@
-const cookbook = {
-    title: "The Book on Pie",
-    author: "Erin Jeanne McDowell",
-    subject: "Baking",
-}
-
-const nonFiction = {
-    title: "Crying in H-Mart",
-    author: "Michelle Zauner",
-    genre: "Memoir",
-}
-
-const fiction = {
-    title: "Cloud Cuckoo Land",
-    author: "Anthony Doerr",
-    genre: "Historical Fiction",
-}
-
-function createBook(book) {
-    const li = document.createElement('li');
-    const h3 = document.createElement('h3');
-    const h4 = document.createElement('h4');
-    const p = document.createElement('p');
-
-    h3.innerText = book.title;
-    h4.innerText = book.author;
-    p.innerText = book.genre;
-
-    li.append(h3, h4, p);
-    document.querySelector('ul').append(li);
-}
-
-function createCookbook(book) {
-    const li = document.createElement('li');
-    const h3 = document.createElement('h3');
-    const h4 = document.createElement('h4');
-    const p = document.createElement('p');
-
-    h3.innerText = book.title;
-    h4.innerText = book.author;
-    p.innerText = book.subject;
-
-    li.className = 'cooking';
-
-    li.append(h3, h4, p);
-    document.querySelector('ul').append(li);
-}
-
-
-createBook(fiction);
-createBook(nonFiction);
-createCookbook(cookbook);
-
-const newBooks = [
-    { 
-        title: "Walk in the Woods",
-        author: "Bill Bryson",
-    },
-    {
-        title: "Just Kids",
-        author: "Patti Smith",
-    },
-    {
-        title: "A Year in Provence",
-        author: "Peter Mayle",
+document.addEventListener("DOMContentLoaded", () => {
+    //Fetch requests
+    function fetchResource(url) {
+        return fetch(url)
+        .then(res => res.json())
     }
-]
 
-function booksUpNext(book) {
-    const li = document.createElement('li');
-    const h3 = document.createElement('h3');
+    //Rendering functions
+    function createCompletedBook(book) {
+        const li = document.createElement('li');
+        const h3 = document.createElement('h3');
+        const h4 = document.createElement('h4');
+        const p = document.createElement('p');
 
-    h3.innerText = book.title;
+        h3.innerText = book.title;
+        h4.innerText = book.author;
+        p.innerText = book.genre;
 
-    li.append(h3);
-    document.getElementById('up-next').append(li);
-}
+        li.append(h3, h4, p);
+        document.querySelector('ul').append(li);
+    }
+
+    function createNextBook(book) {
+        const li = document.createElement('li');
+        const h3 = document.createElement('h3');
+    
+        h3.innerText = book.title;
+    
+        li.append(h3);
+        document.getElementById('up-next').append(li);
+    }
+
+    //Invoke functions
+    fetchResource('http://localhost:3000/complete')
+    .then(books => books.forEach(createCompletedBook))
+
+    fetchResource('http://localhost:3000/next')
+    .then(books => books.forEach(createNextBook))
+    
+
+})
+
+
 
 newBooks.forEach(booksUpNext);
